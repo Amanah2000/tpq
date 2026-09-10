3a79bc604b146ec3098d3efbf6d121d4e8f2906d
wahyudi <wy6865@gmail.com>
2026-09-10 12:02:57 +0700

    fix: komen import supabase yg blm dipake

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { supabase } from './lib/supabaseClient'
+// import { supabase } from './lib/supabaseClient'
 // import { getKeuangan, addKeuangan, hapusDataKeuangan } from './lib/keuanganApi'
 
 function App() {
